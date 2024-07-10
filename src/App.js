f3efe0a2c6d81f858b08dee876cfa48a34042e35
Irs444 <irs786had123@gmail.com>
2024-07-11 02:50:59 +0530

    Signup Login Routing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,37 @@ import React from 'react';
 // import './App.css';
 import Counter from './features/counter/Counter';
 import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:(<Home></Home>)
+  },
+  {
+    path: "/login",
+    element:(<LoginPage></LoginPage>)
+  },
+  {
+    path: "/signup",
+    element:(<SignupPage></SignupPage>)
+  },
+]);
 
 function App() {
   return (
     <div >
      
 
-   <Home></Home>
+     <RouterProvider router={router} />
 
     </div>
   );
