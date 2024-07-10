60cf44c2b55321eb6a7dec75634e53cb6a61e897
Irs444 <irs786had123@gmail.com>
2024-07-11 01:48:13 +0530

    ProductList, Navbar

--- src/features/product-list/productListSlice.js
+++ src/features/product-list/productListSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchCount } from '../counter/counterAPI';
+import { fetchCount } from '../product-list/productListAPI';
 
 const initialState = {
   value: 0,
@@ -15,7 +15,7 @@ export const incrementAsync = createAsyncThunk(
   }
 );
 
-export const counterSlice = createSlice({
+export const productListSlice = createSlice({
   name: 'counter',
   initialState,
   reducers: {
@@ -43,9 +43,9 @@ export const counterSlice = createSlice({
   },
 });
 
-export const { increment } = counterSlice.actions;
+export const { increment } =  productListSlice.actions;
 
 
 export const selectCount = (state) => state.counter.value;
 
-export default counterSlice.reducer;
+export default productListSlice.reducer;
