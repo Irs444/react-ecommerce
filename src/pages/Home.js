0c985885cf6564af2b7b77396eeaac956568dfe4
Irs444 <irs786had123@gmail.com>
2024-07-11 22:56:32 +0530

    modify productlist, datajson

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navbar from '../features/navbar/Navbar'
-import ProductList from '../features/product-list/components/ProductList'
+import ProductList from '../features/product/components/ProductList'
+
 
 
 const Home = () => {
   return (
     <div>
         <Navbar>
-            <ProductList></ProductList>
+           <ProductList></ProductList>
         </Navbar>
     </div>
   )
